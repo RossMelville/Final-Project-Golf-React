ea1fa3a8e35b170fd04c4b5e4707fa60aa894d8f
Ross Melville <rossmelville@hotmail.com>
2017-10-05 13:59:17 +0100

    Added componentDidMount method to the GolfContainer so it is now taking info from api and storing in the state.

--- client/src/containers/GolfContainer.jsx
+++ client/src/containers/GolfContainer.jsx
@@ -4,9 +4,28 @@ class GolfContainer extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      shots: []
+    }
+  }
+
+  componentDidMount() {
+    const url = "http://localhost:3000/api/shots"
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = () => {
+      if(xhr.status === 200) {
+        const data = JSON.parse(xhr.responseText);
+        this.setState({
+          shots: data
+        })
+      }
+    }
+    xhr.send();
   }
 
   render() {
+    console.log(this.state)
     return(
       <section>
         <p>Golf Stuff going here</p>
