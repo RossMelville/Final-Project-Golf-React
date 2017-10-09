6cc0f835a9fad7c0d7144b4dea63b085ddffcb02
Ross Melville <rossmelville@hotmail.com>
2017-10-09 17:06:41 +0100

    Added a start round button to the opening screen.

--- client/src/containers/GolfContainer.jsx
+++ client/src/containers/GolfContainer.jsx
@@ -7,14 +7,17 @@ class GolfContainer extends Component {
     super(props);
     this.state = {
       shots: [],
+      holes: [],
+      rounds: [],
       currentLocation : {latitude: null, longitude: null},
       previousLocation: {latitude: null, longitude: null}
 
     }
+    this.buttonPressed = this.buttonPressed.bind(this)
   }
 
   componentDidMount() {
-    const url = "http://localhost:3000/api/shots"
+    const url = "http://localhost:3000/shots"
     const xhr = new XMLHttpRequest();
     xhr.open('GET', url);
     xhr.onload = () => {
@@ -26,19 +29,46 @@ class GolfContainer extends Component {
       }
     }
     xhr.send();
+
+    const url2 = "http://localhost:3000/shots/7/holes"
+    const xhr2 = new XMLHttpRequest();
+    xhr2.open('GET', url2);
+    xhr2.onload = () => {
+      if(xhr.status === 200) {
+        const data2 = JSON.parse(xhr2.responseText);
+        this.setState({
+          holes: data2
+        })
+      }
+    }
+    xhr2.send();
+
+    const url3 = "http://localhost:3000/shots/7/holes/96/rounds"
+    const xhr3 = new XMLHttpRequest();
+    xhr3.open('GET', url3);
+    xhr3.onload = () => {
+      if(xhr.status === 200) {
+        const data3 = JSON.parse(xhr3.responseText);
+        this.setState({
+          rounds: data3
+        })
+      }
+    }
+    xhr3.send();
   }
 
-  getLocation() {
-
+  buttonPressed() {
+    console.log(this)
   }
+
 
   render() {
-    console.log(this.state.shots)
     return(
       <section>
-        <p>Golf Stuff going here</p>
+        <h1>Golf Shot Tracker</h1>
+        <button onClick={this.buttonPressed}>Start Round</button>
 
-        <Location currentLocation={this.state.currentLocation} previousLocation={this.state.previousLocation}/>
+        
       </section>
     )
   }
