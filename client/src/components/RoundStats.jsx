275fb90b33cbf9bfae0f3ae2e544741683acd1f4
Ross Melville <rossmelville@hotmail.com>
2017-10-10 09:41:50 +0100

    Added to the RoundStats.jsx constructor and render function.

---
+++ client/src/components/RoundStats.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class RoundStats extends Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    if(this.props.state.selectedPage === "roundStats") {
+      return (
+        <section>
+          Round Stats and stuff.
+        </section>
+      )
+    }else{
+      return (
+        null
+      )
+    }
+  }
+
+}
+
+export default RoundStats
